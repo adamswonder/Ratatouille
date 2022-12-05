0c27b396adf6ec5d0ab0318ca23dd2da0b32b4ab
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-12-05 15:52:45 +0300

    adds recipe migration

--- db/migrate/20221205111111_create_recipes.rb
+++ db/migrate/20221205111111_create_recipes.rb
@@ -1,6 +1,9 @@
 class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
+      t.string :title
+      t.text :instructions
+      t.integer :minutes_to_complete
       
       t.timestamps
     end
