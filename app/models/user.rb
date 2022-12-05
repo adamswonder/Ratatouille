a5516d76850c5781d7ecc9142c59344acefeadaf
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-12-05 21:12:03 +0300

    deletes commits

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,4 @@ class User < ApplicationRecord
     has_many :recipes
     has_many :reviews
 
-    # Reviews
-    # belongs_to :user
-     # belongs_to :recipe
-
-
-    #  Recipes
-    # has_many :reviews
-    # belongs_to :user
 end
