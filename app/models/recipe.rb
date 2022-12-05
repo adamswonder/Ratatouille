157b6944d7cf66adca613799082358ed4a8469b0
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-12-05 14:18:21 +0300

    recipe resources

---
+++ app/models/recipe.rb
@@ -0,0 +1,8 @@
+class Recipe < ApplicationRecord
+    belongs_to :user
+
+    # validation
+    validates :title, presence: true
+    validates :instructions, presence: true, length: { minimum: 50 }
+    validates :minutes_to_complete, presence: true
+end
