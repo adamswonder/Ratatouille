cfd8723e3e2bda37fe999cd5b211d64069e186b5
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-12-06 09:37:17 +0300

    suspends validation for user

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,9 @@
 class Recipe < ApplicationRecord
-    has_many :users
+    belongs_to :user
+    has_many :reviews
 
     # validation
-    validates :title, presence: true
-    validates :instructions, presence: true, length: { minimum: 50 }
-    validates :minutes_to_complete, presence: true
+    # validates :title, presence: true
+    # validates :instructions, presence: true, length: { minimum: 50 }
+    # validates :minutes_to_complete, presence: true
 end
