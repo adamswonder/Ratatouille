444855058c947919e3839c1342319f5c3df5f020
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-12-07 23:33:18 +0300

    adds cookies and record validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+    include ActionController::Cookies
+    rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_method
+    
+    def unprocessable_entity_method entity
+        render json: {errors: [entity.record.errors]}, status: :unprocessable_entity
+    end
 end
