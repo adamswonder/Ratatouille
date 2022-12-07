3b6ff549a17e907564316e417bc20d6e19a259d8
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-12-07 22:15:46 +0300

    adds routes to app component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,38 @@
-import Pages from "../pages/Pages";
-import '../index.css'
-
+import React, { useEffect, useState } from "react";
+import { Switch, Route } from "react-router-dom";
+import NavBar from "./NavBar";
+import Login from "../pages/Login";
+import RecipeList from "../pages/RecipeList";
+import NewRecipe from "../pages/NewRecipe";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  if (!user) return <Login onLogin={setUser} />;
+
   return (
-    <div className="App">      
-       <Pages/>       
-    </div>
+    <>
+      <NavBar user={user} setUser={setUser} />
+      <main>
+        <Switch>
+          <Route path="/new">
+            <NewRecipe user={user} />
+          </Route>
+          <Route path="/">
+            <RecipeList />
+          </Route>
+        </Switch>
+      </main>
+    </>
   );
 }
 
