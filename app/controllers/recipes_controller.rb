56b42b23094ecee9fe0b047edaed4365969dd9ec
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-12-08 00:39:10 +0300

    adds helper method

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,29 +1,39 @@
 class RecipesController < ApplicationController
-    rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found_response
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
+    # make sure the user is signed in before showing the resource
     before_action :authorize
     def index
         render json: Recipe.all
     end
-
+    # GET /recipes/:id
+    def show
+        recipe = find_recipe
+        render json: recipe, status: :ok
+    end
+    # POST /recipes
     def create
         user = User.find(session[:user_id])
         recipe = Recipe.create!(user_id: user.id, title: recipe_params[:title], instructions: recipe_params[:instructions], minutes_to_complete: recipe_params[:minutes_to_complete])
         render json: recipe, status: :created
     end
-
+    # DELETE /recipes/:id
     def destroy
-        recipe = Recipe.find(:id)
+        recipe = find_recipe
         recipe.destroy
         head :no_content
     end
-
+    # PATCH /recipes/:id
     def update
-        recipe = Recipe.find(:id)
+        recipe = find_recipe
         recipe.update!(recipe_params)
         render json: recipe, status: :accepted
     end
 
     private
+    def find_recipe
+        Recipe.find_by!(id:params[:id])
+    end
+    
     def recipe_params
         params.permit(:title, :instructions, :minutes_to_complete)
     end
@@ -32,7 +42,7 @@ class RecipesController < ApplicationController
         return render json: {errors: ["Not authorized"]}, status: :unauthorized unless session.include? :user_id
     end
 
-    def render_not_found_response
+    def record_not_found_response
         render json: { errors: "Recipe not found"}, status: :not_found
     end
 end
