b0d6b475b56fceb0d12062c630ea1152d99d7fd4
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-12-07 23:57:23 +0300

    fix users table migration columns

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_122108) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
-    t.string "image_url"
     t.string "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
