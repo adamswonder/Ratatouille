d6e1d00ec7557c464e5e9205588048ad1fed4756
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-12-07 23:33:53 +0300

    adds validation to recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,7 @@ class Recipe < ApplicationRecord
     has_many :reviews
 
     # validation
-    # validates :title, presence: true
-    # validates :instructions, presence: true, length: { minimum: 50 }
-    # validates :minutes_to_complete, presence: true
+    validates :title, presence: true
+    validates :instructions, presence: true, length: { minimum: 50 }
+    validates :minutes_to_complete, presence: true
 end
