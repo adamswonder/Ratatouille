264f2200cd34aa41f3d5fea27fee2a73e14679be
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-12-09 21:47:53 +0300

    fix seed file

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_122108) do
     t.string "title"
     t.text "instructions"
     t.integer "minutes_to_complete"
+    t.string "image_url"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
